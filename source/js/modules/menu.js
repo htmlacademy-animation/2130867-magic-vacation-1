78824e7b24724e756012c73a6fe31ee7b9f31cdf
m.naumkina <m.naumkina@bftcom.com>
2022-09-15 18:43:26 +0300

    2.9. Вкатываем кнопки слайдера

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -17,6 +17,11 @@ export default () => {
 
   for (let i = 0; i < menuLinks.length; i++) {
     menuLinks[i].addEventListener(`click`, function () {
+      document.body.classList.remove(`dark-blue`, `light-blue`, `light-purple`, `dark-purple`);
+
+      if (this.dataset.href === `story`) {
+        document.body.classList.add(`light-purple`);
+      }
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
